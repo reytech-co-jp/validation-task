3762c9e4e53b58423b1f1db47e4d96ac19caf190
renangton <puccho0427@gmail.com>
2023-11-25 19:57:23 +0900

    プロジェクトの作成

---
+++ src/main/java/com/validationtask/ValidationtaskApplication.java
@@ -0,0 +1,13 @@
+package com.validationtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/validationtask/ValidationtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validationtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
